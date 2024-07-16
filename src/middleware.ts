0daa54af2d47ab2e22f086e5f04d263300221e6b
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-07-16 17:15:27 -0300

    feat: add middlewares

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse, NextRequest } from 'next/server';
+import { getUrl } from '../lib/get-url';
+
+export default function middleware(request: NextRequest) {
+  const token = request.cookies.get('authjs.session-token');
+  const pathname = request.nextUrl.pathname;
+
+  if (
+    (pathname.includes('sign-in') && token) ||
+    (pathname.includes('sign-up') && token)
+  ) {
+    return NextResponse.redirect(new URL(getUrl('/app')));
+  }
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$|favicon.ico).*)'],
+};
