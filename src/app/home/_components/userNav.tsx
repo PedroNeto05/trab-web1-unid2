cbd33dffcf1a868e8b4e9a6bd0ab46638f66c317
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-07-25 17:19:43 -0300

    feat: add user info in nav bar

--- src/app/home/_components/userNav.tsx
+++ src/app/home/_components/userNav.tsx
@@ -7,51 +7,59 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { useSession } from 'next-auth/react';
+import { useTheme } from 'next-themes';
+import { signOut } from 'next-auth/react';
 
 export function UserNav() {
+  const { data: session } = useSession();
+  const userFullName = `${session?.user.firstName} ${session?.user.lastName}`;
+  const { setTheme } = useTheme();
+  function getFallbackName(fullName: string) {
+    const names = fullName.split(' ');
+    const initials = names.map((name) => name.charAt(0)).join('');
+    return initials;
+  }
+
+  function handleLogOut() {
+    signOut();
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
           <Avatar className='size-10'>
-            <AvatarImage src='/avatars/01.png' alt='@shadcn' />
-            <AvatarFallback>SC</AvatarFallback>
+            <AvatarImage alt='@shadcn' />
+            <AvatarFallback>{getFallbackName(userFullName)}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className='w-56' align='end' forceMount>
         <DropdownMenuLabel className='font-normal'>
           <div className='flex flex-col space-y-1'>
-            <p className='text-sm font-medium leading-none'>shadcn</p>
+            <p className='text-sm font-medium leading-none'>{userFullName}</p>
             <p className='text-xs leading-none text-muted-foreground'>
-              m@example.com
+              {session?.user.email}
             </p>
           </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
-          <DropdownMenuItem>
-            Profile
-            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          <DropdownMenuItem onClick={() => setTheme('system')}>
+            System Theme
           </DropdownMenuItem>
-          <DropdownMenuItem>
-            Billing
-            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          <DropdownMenuItem onClick={() => setTheme('light')}>
+            Light Theme
           </DropdownMenuItem>
-          <DropdownMenuItem>
-            Settings
-            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          <DropdownMenuItem onClick={() => setTheme('dark')}>
+            Dark Theme
           </DropdownMenuItem>
-          <DropdownMenuItem>New Team</DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          Log out
-          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-        </DropdownMenuItem>
+        <DropdownMenuItem onClick={handleLogOut}>Log out</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
